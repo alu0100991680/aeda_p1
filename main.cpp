2d53fde899359c8a0dcd6bfa62baf35178b932f6
noc <noc@noc-B150-HD3P>
2018-02-10 21:33:51 +0000

    Incluimos una estructura de datos ejemplo TDATO

--- main.cpp
+++ main.cpp
@@ -5,13 +5,37 @@
  * Created on 10 de febrero de 2018, 21:07
  */
 
-#include <cstdlib>
+// class templates
+// function template
 #include <iostream>
-
 using namespace std;
 
-int main() 
+/*
+template <typename T>
+class TDATO
 {
-    cout << "Hello, World!";
+    T* _ptr;
+
+    public:
+        explicit TDATO(T* ptr = 0) : _ptr(ptr) {}
+        ~TDATO() { delete _ptr; }
+
+        // implementamos estos operadores para que la clase actue como puntero
+        T& operator*() { return *_ptr; }
+        T* operator->() { return _ptr; }
+};*/
+
+struct TDATO
+{
+    int cod;
+    char name[100];
+    char surname[100];
+};
+
+int main()
+{
+    
     return 0;
 }
+
+
