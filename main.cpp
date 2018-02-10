fcdb4f6a272e996382a463dd1ec0209297ebeb83
noc <noc@noc-B150-HD3P>
2018-02-10 21:12:24 +0000

    Primeros pasos con el IDE

---
+++ main.cpp
@@ -0,0 +1,17 @@
+/* 
+ * File:   main.cpp
+ * Author: noc
+ *
+ * Created on 10 de febrero de 2018, 21:07
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+    cout << "Hello, World!";
+    return 0;
+}
